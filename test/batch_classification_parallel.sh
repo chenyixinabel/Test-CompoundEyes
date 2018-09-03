aac23c2a8901cdc6bc1f361412ab0a6c20f620f9
yixin <chenyixinabel@gmail.com>
2018-09-03 11:28:33 +0800

    Remove the section for deleting the videos in the Test_vs directories

--- test/batch_classification_parallel.sh
+++ test/batch_classification_parallel.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-#Usage ./batch_classification_parallel.sh <classifiers location> <dataset location>
+#Usage ./batch_classification_parallel.sh <program location> <dataset location>
 
 for((i=1;i<=24;i++))
 do
+echo "Running the program on Group "$i":"
 ./run_classifiers_parallel.sh $1 $2 $i
 done
